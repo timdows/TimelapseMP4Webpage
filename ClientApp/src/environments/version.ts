52552667324c4861d295f4b621d29d98b08d5eee
Timdows <tim@inexpro.nl>
2018-06-16 14:07:49 +0200

    Create publish task

--- ClientApp/src/environments/version.ts
+++ ClientApp/src/environments/version.ts
@@ -1,10 +1,10 @@
 
 export const appVersion = '0.0.0';
 
-export const gitShort = 'e777bab';
+export const gitShort = 'e9ce9e1';
 
-export const gitLong = 'e777bab6116ae0dd615a936d7a31800a03e9b161';
+export const gitLong = 'e9ce9e1a609529d0249023b7abbff02a7b235a35';
 
-export const gitDate = 'Sat Jun 16 2018 12:58:37 GMT+0200 (W. Europe Daylight Time)';
+export const gitDate = 'Sat Jun 16 2018 13:15:45 GMT+0200 (W. Europe Daylight Time)';
 
 export const gitBranch = 'master';
