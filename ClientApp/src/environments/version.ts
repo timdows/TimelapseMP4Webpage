5a1187fbec8c971bee192661b794ab6000b253ac
Timdows <tim@inexpro.nl>
2018-06-17 11:03:36 +0200

    Main page background and title

--- ClientApp/src/environments/version.ts
+++ ClientApp/src/environments/version.ts
@@ -1,10 +1,10 @@
 
 export const appVersion = '0.0.0';
 
-export const gitShort = 'cc25245';
+export const gitShort = '28ed7d1';
 
-export const gitLong = 'cc25245b1ffd713b6f9f7b86f311fedf268bcac3';
+export const gitLong = '28ed7d17b06192c9f4e8cae8b733e84befd1de84';
 
-export const gitDate = 'Sat Jun 16 2018 14:22:47 GMT+0200 (W. Europe Daylight Time)';
+export const gitDate = 'Sun Jun 17 2018 10:20:19 GMT+0200 (W. Europe Daylight Time)';
 
 export const gitBranch = 'master';
