7aa42a26f05d17efa6b7fc62882362f024553fa3
Timdows <tim@inexpro.nl>
2018-06-17 11:29:47 +0200

    Hover effects

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.pug',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.pug',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 }
