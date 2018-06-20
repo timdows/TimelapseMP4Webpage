f6b686f891833b9ff84e3cfa9e3c9dbaa06dbf04
Timdows <tim@inexpro.nl>
2018-06-20 19:32:29 +0200

    Latest changes

--- ClientApp/src/environments/version.ts
+++ ClientApp/src/environments/version.ts
@@ -1,10 +1,10 @@
 
 export const appVersion = '0.0.0';
 
-export const gitShort = 'd701406';
+export const gitShort = '7500f42';
 
-export const gitLong = 'd701406564224fd0bddcb9d4072a94ada947cf26';
+export const gitLong = '7500f426c34046ed14e84329e52fc2fc847cc74a';
 
-export const gitDate = 'Wed Jun 20 2018 14:01:43 GMT+0200 (W. Europe Daylight Time)';
+export const gitDate = 'Wed Jun 20 2018 19:25:17 GMT+0200 (W. Europe Daylight Time)';
 
 export const gitBranch = 'master';
