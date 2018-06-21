e5a62bfebea0ad864f6226e74d61cee4a8b60a55
Timdows <tim@inexpro.nl>
2018-06-21 23:52:49 +0200

    Upload image

--- Controllers/Hour1400Controller.cs
+++ Controllers/Hour1400Controller.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using TimelapseMP4Webpage.Models;
 
 namespace TimelapseMP4Webpage.Controllers
@@ -27,7 +29,7 @@ namespace TimelapseMP4Webpage.Controllers
 				.Where(item => item.FileName.EndsWith("_thumb.jpg"))
 				.OrderBy(item => item.DateTaken)
 				.ToList();
-			return Json(images);	
+			return Json(images);
 		}
 
 		public IActionResult GetImage(string fileName)
@@ -40,6 +42,25 @@ namespace TimelapseMP4Webpage.Controllers
 			}
 
 			return NotFound();
+		}
+
+		public async Task<IActionResult> Upload(IFormFile file, string fileName, string secret)
+		{
+			// full path to file in temp location
+			var filePath = Path.GetTempFileName();
+
+			if (file.Length > 0)
+			{
+				using (var stream = new FileStream(filePath, FileMode.Create))
+				{
+					await file.CopyToAsync(stream);
+				}
+			}
+
+			// process uploaded files
+			// Don't rely on or trust the FileName property without validation.
+
+			return Ok(new { file.Length, filePath });
 		}
 	}
 }
